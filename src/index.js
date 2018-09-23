375de886b18ff8d051ce8870b514e9ad566fb00c
803142 <803142@gmail.com>
2018-09-23 05:32:22 +0300

    work

--- src/index.js
+++ src/index.js
@@ -1,30 +1,45 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.array = [];
+    this.compareFunction = (a, b) => a-b;
   }
 
   add(element) {
     // your implementation
+    this.array.push(element);
   }
 
   at(index) {
     // your implementation
+    return this.array[index];
   }
 
   get length() {
     // your implementation
+    return this.array.length;
   }
 
   toArray() {
     // your implementation
+    return this.array;
   }
 
   sort(indices) {
     // your implementation
-  }
-
+    let tempArray = []; 
+    indices.sort((a, b) => a-b); 
+    for (var i = 0; i < indices.length; i++) {
+      tempArray.push(this.array[indices[i]]);
+      }
+      tempArray.sort(this.compareFunction);
+      for (var i = 0; i < tempArray.length; i++) {
+        this.array[indices[i]] = tempArray[i];
+    }
+    }
   setComparator(compareFunction) {
     // your implementation
+    this.compareFunction = compareFunction; 
   }
 }
 
